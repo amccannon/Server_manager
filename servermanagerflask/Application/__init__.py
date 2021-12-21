0de0f2c0895108d79e6fe644e05497f434ecc121
Antonino <abeshia@my.erau.edu>
2021-12-21 10:23:21 -0500

    latest 12/21/20021
    ssdgsg

--- servermanagerflask/Application/__init__.py
+++ servermanagerflask/Application/__init__.py
@@ -15,6 +15,11 @@ from wtforms import *
 app = Flask(__name__)
 app.secret_key = 'replace later'
 
+# - POSTGRES_USER= postgres
+# - POSTGRES_PW= testing1
+# - POSTGRES_URL= main-database.chkovmh0wpxg.us-east-2.rds.amazonaws.com
+# - POSTGRES_DB= Login/Info
+
 # this can be the root directory (decorators using these different types of urls)
 
 # importing routes from Application
@@ -31,37 +36,14 @@ db.init_app(app)
 migrate = Migrate(app, db)
 
 
-class User(db.Model):
-    """User model"""
-
-    #give the table a name
-
-    __tablename__ = "members"
-
-    id = db.Column(db.Integer, primary_key=True)  # ID
-    firstname = db.Column(db.String(25), unique=True, nullable=False) #first name
-    lastname = db.Column(db.String(25), unique=True, nullable=False) #lastname name
-    email = db.Column(db.String(25), unique=True, nullable=False) #email
-    password = db.Column(db.String(25), unique=True, nullable=False) #password
-    team = db.Column(db.Integer, unique=True, nullable=False) #team
-    
-
-
-    def __init__(self, firstname: str, lastname: str, email: str, password: str, team: int):
-        self.firstname = firstname
-        self.lastname = lastname
-        self.email = email
-        self.password = password
-        self.team = team
 
 
 
-
-t_host = "main-database.chkovmh0wpxg.us-east-2.rds.amazonaws.com"
+#t_host = "main-database.chkovmh0wpxg.us-east-2.rds.amazonaws.com"
 #t_host = "postgresql+psycopg2://main-database.chkovmh0wpxg.us-east-2.rds.amazonaws.com:5432"
-t_port = "5432"
-t_dbname = "postgres"
-t_user = "postgres"
-t_pw = "testing1"
-db_conn = psycopg2.connect(host=t_host, port=t_port, dbname=t_dbname, user=t_user, password=t_pw)
-db_cursor = db_conn.cursor()
+#t_port = "5432"
+#t_dbname = "postgres"
+#t_user = "postgres"
+#t_pw = "testing1"
+#db_conn = psycopg2.connect(host=t_host, port=t_port, dbname=t_dbname, user=t_user, password=t_pw)
+#db_cursor = db_conn.cursor()
--- servermanagerflask/Application/models.py
+++ servermanagerflask/Application/models.py
@@ -7,3 +7,27 @@ from flask_migrate import Migrate
 db = SQLAlchemy
 
 #define a class for the model
+class User(db.Model):
+    """User model"""
+
+    #give the table a name
+
+    __tablename__ = "members"
+
+    id = db.Column(db.Integer, primary_key=True)  # ID
+    firstname = db.Column(db.String(25), unique=True, nullable=False) #first name
+    lastname = db.Column(db.String(25), unique=True, nullable=False) #lastname name
+    email = db.Column(db.String(25), unique=True, nullable=False) #email
+    password = db.Column(db.String(25), unique=True, nullable=False) #password
+    team = db.Column(db.Integer, unique=True, nullable=False) #team
+
+    db.create_all()
+    
+
+
+    #def __init__(self, firstname: str, lastname: str, email: str, password: str, team: int):
+    #    self.firstname = firstname
+    #   self.lastname = lastname
+    #    self.email = email
+    #    self.password = password
+    #    self.team = team
